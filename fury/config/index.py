8b5d81265b41097a76226485e68799e200a61bf7
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 10:27:57 +0000

    feat(api): fix logging interface

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_162cde6e78bd40dfb7245f7a95b7bcba = () => {
-  return '2025-02-17T14:04:16Z';
+const node_663e3f25796c4da99c0b4fe51d8e406b = () => {
+  return '2025-03-01T10:27:57Z';
 };
