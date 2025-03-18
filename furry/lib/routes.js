3b9e40a037955d5063d53ceeafa177a37286eeb7
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-18 22:18:07 +0000

    feat(utils): fix database index

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab1914e801304cacbcb8e587a267381f = () => {
-  return '2025-03-07T05:13:27Z';
+const node_c9cb0d4ec1e24ec09002c426a2315793 = () => {
+  return '2025-03-18T22:18:07Z';
 };
