adf701def99b5f0ff6c991511a685489dfa8bb5e
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-18 18:55:12 +0000

    test(auth): decouple logging interface

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e0ce9b690494e5aa99465c42d6e8877 = () => {
-  return '2025-03-16T18:18:30Z';
+const node_7efa0988e72c4033bd1422b38aba3831 = () => {
+  return '2025-03-18T18:55:12Z';
 };
