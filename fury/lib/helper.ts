336c4e0ff00904c94abfee38b5355243c05a4af4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 06:29:26 +0000

    test(worker): restructure logging interface

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_335363513fa8437aaa5f203627ea9eef = () => {
-  return '2025-03-28T20:21:03Z';
+const node_b1efb25a03194a22a8e134102ac34d65 = () => {
+  return '2025-04-04T06:29:26Z';
 };
