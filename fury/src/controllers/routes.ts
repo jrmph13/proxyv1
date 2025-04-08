ea05ef8f20aba6d5abc31b1d3bfeac228b9bbd2b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 08:04:03 +0000

    feat(utils): decouple payload parser

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d3ea3de59464521ad839324586f77db = () => {
-  return '2025-03-07T17:43:15Z';
+const node_de735b9c360c4ae8b854f4c0ec35d42d = () => {
+  return '2025-04-08T08:04:03Z';
 };
