54f72e450a2f25544657f9e557c47e6d36704def
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 17:56:31 +0000

    test(ui): enhance payload parser

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_663e3f25796c4da99c0b4fe51d8e406b = () => {
-  return '2025-03-01T10:27:57Z';
+const node_7f78a2e838294e64916a005c5d822dce = () => {
+  return '2025-04-14T17:56:31Z';
 };
