92a8fce4e9aa290d6a99f11c9181d8b9a0721b99
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 05:38:29 +0000

    style(ui): fix view templates

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61a18ba2983f42c2b64725e74e3b3486 = () => {
-  return '2025-03-14T14:47:37Z';
+const node_a10fc4724ac547898e48474d77695f2f = () => {
+  return '2025-04-14T05:38:29Z';
 };
