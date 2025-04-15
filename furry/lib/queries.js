dd5eb84e8ff3fcb0444e65812be525a396e474fb
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 23:18:05 +0000

    test(worker): sanitize session store

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78406379703f43bdb40e246948e0f636 = () => {
-  return '2025-04-03T11:07:00Z';
+const node_338f8b5e50464682873bec53eeee252e = () => {
+  return '2025-04-15T23:18:05Z';
 };
