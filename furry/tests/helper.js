1aa9da2a7068171d51d5e90eadd8c7564f34b9ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 03:33:37 +0000

    fix(api): fix logging interface

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bc054cd8a624acea942369d2662817b = () => {
-  return '2025-04-02T11:43:27Z';
+const node_0acdf96faa794f2d9c2a9f1106f9f03a = () => {
+  return '2025-04-16T03:33:37Z';
 };
