4c2d3d88b92b83ff2ffab44321fecec7f135b635
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 17:19:10 +0000

    fix(config): update validation pipeline

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe656d4b137e444bae72754f3edcde88 = () => {
-  return '2025-02-28T20:14:10Z';
+const node_87659ee869be487581188912f5a93bc2 = () => {
+  return '2025-04-16T17:19:10Z';
 };
