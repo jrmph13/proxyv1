3e622b61ead561e9c6ae377c7d89abc0d3bb7b2c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 07:59:51 +0000

    feat(api): refactor view templates

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_773ca35baa2f4562a506c797fea12945 = () => {
-  return '2025-04-09T21:39:34Z';
+const node_9c74494083f94e12a9ada2cd219a06e5 = () => {
+  return '2025-04-17T07:59:51Z';
 };
