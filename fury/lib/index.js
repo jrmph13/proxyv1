f00f286ca787362c47223801c04ae5e18d8f2a64
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 01:43:17 +0000

    feat(auth): optimize view templates

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8059d68a0608431882a4179232fd6242 = () => {
-  return '2025-03-14T21:56:19Z';
+const node_311c9351a58041dd81b273604325262e = () => {
+  return '2025-04-18T01:43:17Z';
 };
