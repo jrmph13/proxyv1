aa9d67b08db7b4cab6147444cd284dcc77c50bd1
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 23:56:44 +0000

    docs(worker): restructure caching layer

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd8cf53368144b2ea1bd7b721ce129e9 = () => {
-  return '2025-04-16T17:20:27Z';
+const node_8c3078383e6049f0aa4f2321076c551a = () => {
+  return '2025-04-24T23:56:44Z';
 };
