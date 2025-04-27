da0f2ba43d3e480aa13cfe82386fa642fa2406bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 00:45:08 +0000

    test(ui): fix view templates

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be1a41f1036541378be6d81addf2ddf6 = () => {
-  return '2025-04-14T16:17:10Z';
+const node_13d948677da142dc8bafd7061d3eb227 = () => {
+  return '2025-04-27T00:45:08Z';
 };
