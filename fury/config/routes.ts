f8f84a6b9808c8522c503fb326acf96cc5bd8cd3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 15:23:49 +0000

    perf(auth): fix validation pipeline

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7312c95c1b7f43cda0faace9c8e983aa = () => {
-  return '2025-04-17T22:21:52Z';
+const node_a3e8372fbad74dc890ff9c4d871870d6 = () => {
+  return '2025-04-28T15:23:49Z';
 };
