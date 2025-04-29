25374969af05e612f8a2f569d087a6ed40f823be
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 15:58:37 +0000

    fix(worker): sanitize auth tokens

--- furry/src/components/validator.js
+++ furry/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cc616a0e39c4b61b572e2ed606cf457 = () => {
-  return '2025-03-04T18:35:38Z';
+const node_12576af25d8b40ed868254df9992e160 = () => {
+  return '2025-04-29T15:58:37Z';
 };
