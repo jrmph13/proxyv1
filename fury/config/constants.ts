fb41419f397632e0262039725525e3c26350c844
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 23:31:18 +0000

    refactor(auth): restructure database index

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58760c528ef846f7bd7251fa359271f0 = () => {
-  return '2025-02-16T10:41:26Z';
+const node_3b1d54aed8d84b1bb58a9149e4896543 = () => {
+  return '2025-04-30T23:31:18Z';
 };
