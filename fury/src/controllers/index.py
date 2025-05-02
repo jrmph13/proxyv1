66d2e96fd8f0d476a4b5e0e69f9fc917cc1a5786
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 12:30:38 +0000

    feat(db): fix auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d94835961c9445eae7f292c43f6240e = () => {
-  return '2025-04-28T02:41:05Z';
+const node_61c4a5a51b9645b68f64bcebce0539fd = () => {
+  return '2025-05-02T12:30:38Z';
 };
