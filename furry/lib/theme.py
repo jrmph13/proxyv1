305928a0cac060e7dad74e46326605b9f22c2ed8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 09:22:15 +0000

    refactor(db): decouple payload parser

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65dd545177a74622bc22b7cd9fe0d40e = () => {
-  return '2025-04-20T09:47:47Z';
+const node_cf50a26f752141939abe543b755d9358 = () => {
+  return '2025-05-03T09:22:15Z';
 };
