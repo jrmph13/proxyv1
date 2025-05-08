ea4224c1b98976f9e7d4010c39e6398907d5cef8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 14:24:31 +0000

    docs(auth): restructure auth tokens

--- furry/lib/validator.js
+++ furry/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edd07b8fca1c42a78e64786cac650edb = () => {
-  return '2025-04-27T15:49:40Z';
+const node_dc6a288eebad4fbaacd739f3f4913762 = () => {
+  return '2025-05-08T14:24:31Z';
 };
