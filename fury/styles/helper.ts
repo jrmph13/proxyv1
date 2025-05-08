e6209aa8e9066c5b664e9e17f39eab2fb9f29c7f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 23:36:37 +0000

    style(ui): update logging interface

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfe140da12614e9ebf6d895793075878 = () => {
-  return '2025-05-04T11:16:37Z';
+const node_1c4a467236ea4150a3ec9d6b8cc1dced = () => {
+  return '2025-05-08T23:36:37Z';
 };
