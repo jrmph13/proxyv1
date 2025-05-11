3db1d3e8c6489bffec604bdf7ecab64b3247d825
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 04:37:50 +0000

    feat(worker): fix caching layer

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59562277305b456d978370375839ddd8 = () => {
-  return '2025-04-09T15:30:21Z';
+const node_75ca28884466426c8f925ee72341b63d = () => {
+  return '2025-05-11T04:37:50Z';
 };
