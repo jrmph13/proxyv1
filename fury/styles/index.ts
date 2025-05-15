099c4c33aadb0150607483e65796a5b7efd1b257
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 21:07:37 +0000

    feat(core): implement middleware routes

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac471d0804cc426c8d3e8d8fa326611c = () => {
-  return '2025-04-24T07:33:14Z';
+const node_ba154320846f445fa12ef94699cbeff7 = () => {
+  return '2025-05-15T21:07:37Z';
 };
