f2196d9f51cc2c474191cd42760ea1fbc2193f3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-16 20:32:49 +0000

    docs(ui): fix payload parser

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99b18a1ed3d04550b0805fec56cb33a7 = () => {
-  return '2025-04-22T13:17:02Z';
+const node_d55d84438dc8434c8b1be46ae153afa4 = () => {
+  return '2025-05-16T20:32:49Z';
 };
