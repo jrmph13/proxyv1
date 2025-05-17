06443319f2726db9ac31506f2f1ac707a3f2459b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 17:42:33 +0000

    feat(db): decouple payload parser

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_774bacce244042cf96ec4df24853e034 = () => {
-  return '2025-05-16T13:15:51Z';
+const node_85bf9751893e4ef49819524c7bd3d8d6 = () => {
+  return '2025-05-17T17:42:33Z';
 };
