75ac618d4ef72e055ce23581fc03eb42bb193f12
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 02:30:51 +0000

    refactor(deps): sanitize logging interface

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ea81da86b5549fe8c749395c2b1f6a6 = () => {
-  return '2025-04-24T13:22:05Z';
+const node_e010c92c720847688ec0a8e1f636664c = () => {
+  return '2025-05-17T02:30:51Z';
 };
