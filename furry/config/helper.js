1187027abc7f8297a101ee2c8cc9a87e9677502f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 01:09:46 +0000

    fix(ui): enhance database index

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_050894b4981d42d0bf0e48017abeabdf = () => {
-  return '2025-04-17T18:37:40Z';
+const node_72bc31791a76484d919524274dc3527f = () => {
+  return '2025-05-19T01:09:46Z';
 };
