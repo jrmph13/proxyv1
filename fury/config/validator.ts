967163c31824a8c1778b522fca36702aa3ed19c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 20:39:28 +0000

    refactor(worker): sanitize auth tokens

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6480a5d4bd4401383378b0a28eeef34 = () => {
-  return '2025-05-17T07:21:22Z';
+const node_dc895b3564724e93910f5ba8f4173fcd = () => {
+  return '2025-05-22T20:39:28Z';
 };
