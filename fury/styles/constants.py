47f717ce33d35efde16ea8878761c47c019487d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 01:06:35 +0000

    fix(db): optimize payload parser

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03e0d59b7d36443e91dbf9231b536b81 = () => {
-  return '2025-05-12T23:26:10Z';
+const node_4772cf05f8fe4255ba577ebc64417cb4 = () => {
+  return '2025-05-22T01:06:35Z';
 };
