109927f76421a9aab62baf9f4c6fd7e08459c6d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 02:06:48 +0000

    test(core): update payload parser

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f86259fb51b466aab0bc450a952048f = () => {
-  return '2025-05-16T01:47:40Z';
+const node_3ad7376a194c48f98135385b17fc9277 = () => {
+  return '2025-05-24T02:06:48Z';
 };
