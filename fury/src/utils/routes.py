25c1a0001a90cf4848ce8af16fc45cdb3aef288b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 14:14:27 +0000

    fix(core): sanitize logging interface

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d55d84438dc8434c8b1be46ae153afa4 = () => {
-  return '2025-05-16T20:32:49Z';
+const node_587e061d4db0490faf94291ffdc80afd = () => {
+  return '2025-05-25T14:14:27Z';
 };
