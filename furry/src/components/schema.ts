6990365dc3ad41729460c5e64bac71c4735019a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 12:55:05 +0000

    style(worker): decouple payload parser

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_64990e4f66a64c4693ccc874d70e3ae1 = () => {
-  return '2025-04-17T22:30:29Z';
+const node_a6b3957ca98d476180fdbd5233d90c54 = () => {
+  return '2025-05-25T12:55:05Z';
 };
