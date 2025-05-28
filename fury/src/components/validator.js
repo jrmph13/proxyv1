249d2d9c7a20e464ae469d4924d2b22ba72c1479
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 03:40:48 +0000

    fix(auth): restructure middleware routes

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e00c9ba3f91e4cfdb96d27880f7578a4 = () => {
-  return '2025-05-15T11:11:15Z';
+const node_3f199a4c09e74512a57fab56b018616b = () => {
+  return '2025-05-28T03:40:48Z';
 };
