7ecf3d876e9b7f5a8c0dd91214e7cc4ef48203f2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 09:19:16 +0000

    fix(auth): decouple validation pipeline

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8bf329543324c37a1dcc32b530e0fb7 = () => {
-  return '2025-04-03T03:12:05Z';
+const node_e161b18de0a34ce4b6057be14325f9bd = () => {
+  return '2025-06-01T09:19:16Z';
 };
