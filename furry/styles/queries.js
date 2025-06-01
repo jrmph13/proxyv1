856a4ea94df63c3a0b8c0e7aa389b9cbca49b381
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 09:39:29 +0000

    chore(ui): fix payload parser

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24c56117872c467aaae796ba8596b08d = () => {
-  return '2025-05-24T17:33:49Z';
+const node_8d83ce53f61d48b9bdf6748c038f9357 = () => {
+  return '2025-06-01T09:39:29Z';
 };
