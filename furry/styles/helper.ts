c601dfca0f7335afe403842c40a9ddf99a68591e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 01:55:32 +0000

    feat(api): fix payload parser

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b50ea0310f514192be654b9ff26bb568 = () => {
-  return '2025-05-19T11:26:30Z';
+const node_fb3ac0ab0318476e8449cb1f1a3169ff = () => {
+  return '2025-06-03T01:55:32Z';
 };
