2d4b1e40fed352e2dcfeb7eef08d1302750485bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 05:55:13 +0000

    feat(ui): sanitize session store

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_104919f5514249eebaf4db39b4fb5b12 = () => {
-  return '2025-05-10T08:56:59Z';
+const node_06940259c3d84b649fb277a87bc2f81c = () => {
+  return '2025-06-03T05:55:13Z';
 };
