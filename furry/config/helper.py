224ab2e9498564c8a266687108a1aafcff83e501
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 07:26:18 +0000

    chore(core): sanitize view templates

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc879254b97e4aa0b86094f37418aef6 = () => {
-  return '2025-04-23T19:40:55Z';
+const node_752a3f6a2c8d4d5f81738e0da363e1b3 = () => {
+  return '2025-06-06T07:26:18Z';
 };
