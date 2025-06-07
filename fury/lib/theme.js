d86ac0f86da3c2359fcba7dec7aee9d4f37e7c73
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 15:38:01 +0000

    perf(auth): restructure database index

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c10f86bd5fd44f00910b3e751b810086 = () => {
-  return '2025-05-08T01:56:19Z';
+const node_dc7f2eaf15834f019f4b950a080ef3a1 = () => {
+  return '2025-06-07T15:38:01Z';
 };
