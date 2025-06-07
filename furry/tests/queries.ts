cc6865b2f7a757e715ef541b95b25bc6c5900887
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 10:25:57 +0000

    style(core): refactor caching layer

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5a0b3eb33fc43ee9c241de2284f18b4 = () => {
-  return '2025-04-11T03:05:33Z';
+const node_7f1715a40ce441f4b700e5b7d9b73cef = () => {
+  return '2025-06-07T10:25:57Z';
 };
