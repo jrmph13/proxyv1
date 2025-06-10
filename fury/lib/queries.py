bc83c32a6bc62a7716ca6e94b733387bd9de5bec
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 13:06:39 +0000

    fix(utils): restructure view templates

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e21c33cb04ff4be4a1439ffc23ac5459 = () => {
-  return '2025-05-11T22:12:05Z';
+const node_3e33bd5c494945bdbe81122992e07b73 = () => {
+  return '2025-06-10T13:06:39Z';
 };
