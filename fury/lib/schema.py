61e2aa16e03e7b5a0909b6956b10a8622eaeca00
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 08:02:30 +0000

    test(db): fix payload parser

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e67d35eb147d44a7a9d1f4640292b880 = () => {
-  return '2025-05-03T16:13:48Z';
+const node_ddb44bfbecfe4266a826f8205927c16d = () => {
+  return '2025-06-13T08:02:30Z';
 };
