f3e4f12b779eb03f451144ecbdd0ba77de06c570
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 14:13:09 +0000

    perf(core): optimize session store

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7433c6a270a4cff89989038791241a7 = () => {
-  return '2025-06-04T02:42:29Z';
+const node_6037ab460385476b910aa2dcb9044cee = () => {
+  return '2025-06-13T14:13:09Z';
 };
