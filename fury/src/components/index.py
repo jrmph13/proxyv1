491969c058ca2461543e57687cdd8b01b51e7465
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 12:10:08 +0000

    perf(db): optimize middleware routes

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6654a481119b4d2d81d8b9ff17b620ba = () => {
-  return '2025-05-23T22:22:36Z';
+const node_ea875d9dfb0f4f78af0af3d10f487d51 = () => {
+  return '2025-06-13T12:10:08Z';
 };
