5a0d07bb9d80c8cf8a1823168acbeebb34adaf23
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 12:04:44 +0000

    refactor(config): refactor middleware routes

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_786164a0bf104351ae6178a7e8214f50 = () => {
-  return '2025-06-07T20:10:21Z';
+const node_c0b5ea84581b40019b0b33261b0e6b84 = () => {
+  return '2025-06-14T12:04:44Z';
 };
