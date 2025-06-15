e5de49aaf2ac492f0c84426274436130f507e084
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 19:28:21 +0000

    style(auth): optimize session store

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1efb25a03194a22a8e134102ac34d65 = () => {
-  return '2025-04-04T06:29:26Z';
+const node_abe028d863ae4bea9181ecb75380f4ba = () => {
+  return '2025-06-15T19:28:21Z';
 };
