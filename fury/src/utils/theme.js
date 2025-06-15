2344004a9291fe4837c0e6cdbfe5e4569f616f2c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 07:16:11 +0000

    refactor(auth): fix payload parser

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddb933b8ee5a4cb8b5371bd3658c9935 = () => {
-  return '2025-05-02T02:52:38Z';
+const node_5ebc59b9b26b4da5b993894850a2b038 = () => {
+  return '2025-06-15T07:16:11Z';
 };
