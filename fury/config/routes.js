e194bf3392853d45c0c4f21a60659384b9d585ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 15:56:28 +0000

    refactor(ui): optimize database index

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c5ce29731d44ffcb44056065c0d76be = () => {
-  return '2025-04-30T22:23:08Z';
+const node_e0155f7bb80948daaad71c4b273fd01f = () => {
+  return '2025-06-15T15:56:28Z';
 };
