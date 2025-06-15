3fb29b1a7996f39b203db9b6a0311bcf57bf6b7f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 02:16:50 +0000

    docs(db): decouple payload parser

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17ca032675dd4ab9954494f81cc4691f = () => {
-  return '2025-04-26T02:29:55Z';
+const node_27270851a1ef4f5a8109980645acba01 = () => {
+  return '2025-06-15T02:16:50Z';
 };
