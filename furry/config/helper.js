cd096b0de1330230851202b7c8b70344b1349373
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-18 22:18:51 +0000

    docs(db): sanitize auth tokens

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d60856a9c3f459ca38a0bc94f3b1bac = () => {
-  return '2025-06-15T06:22:44Z';
+const node_ec24e99eada64c2e9daa0979eb2ed29e = () => {
+  return '2025-06-18T22:18:51Z';
 };
