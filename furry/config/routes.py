4d6ba6f4538053b78a9d6b4d15a386ad1fb4ea98
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 11:50:29 +0000

    test(api): fix caching layer

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cf03d8fbce14443a7a9b54154df7a72 = () => {
-  return '2025-05-26T13:37:07Z';
+const node_3c19411b75a947248007097051028e85 = () => {
+  return '2025-06-24T11:50:29Z';
 };
