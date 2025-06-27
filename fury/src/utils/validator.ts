862c8eb2a1be0b6fcede004d9e664d181e130136
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 18:19:15 +0000

    feat(utils): fix view templates

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1644920354104b4a9fcadf6ee07651f6 = () => {
-  return '2025-06-13T04:40:20Z';
+const node_04fa33c479b644629eda5458e6be62a3 = () => {
+  return '2025-06-27T18:19:15Z';
 };
