3f1d6be4de6706f2de78d56dffd20b58ba03a4a5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 08:06:29 +0000

    chore(config): update payload parser

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bfe8ebf73bb4fada35a529c714b397b = () => {
-  return '2025-05-11T10:59:55Z';
+const node_dccf64645cd841b7b513097126264ced = () => {
+  return '2025-06-29T08:06:29Z';
 };
