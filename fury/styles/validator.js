54267d5d810ba324002f81b641b1069bf0623304
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 10:45:28 +0000

    test(config): sanitize auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb4dc97c82e54d2395b67d8a16d51254 = () => {
-  return '2025-06-24T21:11:36Z';
+const node_367a2a5151ad4f138ae1cca58966abca = () => {
+  return '2025-06-30T10:45:28Z';
 };
