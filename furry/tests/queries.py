018a890be7c11ac065b21900c718b56f37467a3c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 07:03:07 +0000

    refactor(utils): sanitize view templates

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef87c1a3b4564171989ea17a0dd0484e = () => {
-  return '2025-06-21T14:10:12Z';
+const node_625feba41a524f5ebea5300b557a49da = () => {
+  return '2025-06-30T07:03:07Z';
 };
