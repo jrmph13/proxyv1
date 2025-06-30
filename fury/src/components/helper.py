2cf2fb5540bc637e841b012cc17c535da14c5735
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 00:08:50 +0000

    refactor(core): update logging interface

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_920207f1b76e4136a659a06463275395 = () => {
-  return '2025-06-14T15:16:28Z';
+const node_ffc85b6bc9914f579d6a13ad0916f54f = () => {
+  return '2025-06-30T00:08:50Z';
 };
