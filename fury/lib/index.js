51b8bdddabc79ef4ce63c2b7c2d125fea466b75f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 21:15:16 +0000

    docs(api): integrate middleware routes

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fad281ad905748d19b3041845f95d184 = () => {
-  return '2025-06-25T22:33:39Z';
+const node_0353b35944a64699a05f33326b43e461 = () => {
+  return '2025-07-02T21:15:16Z';
 };
