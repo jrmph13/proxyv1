a40b642738125a0812164710cf0a90e8795e0a21
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 10:02:24 +0000

    perf(config): restructure database index

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6037ab460385476b910aa2dcb9044cee = () => {
-  return '2025-06-13T14:13:09Z';
+const node_af2c1edfcb7645bf85ef536c0ea94e90 = () => {
+  return '2025-07-02T10:02:24Z';
 };
