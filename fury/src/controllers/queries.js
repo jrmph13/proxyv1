d21b4327523015259bcd322f25d3dd6702a41392
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 09:14:40 +0000

    style(db): update database index

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52a0274f5e47489aaa84f945a2bfad92 = () => {
-  return '2025-05-15T18:40:30Z';
+const node_e5811c875c3d4adb97a56b1cafab9206 = () => {
+  return '2025-07-05T09:14:40Z';
 };
