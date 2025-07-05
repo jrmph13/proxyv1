cd3be2d7125bbc3e2b608d6cfacc3988a9fdd025
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 06:26:55 +0000

    fix(db): restructure session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94fd365fdc7146e19ccf2ad40bb73670 = () => {
-  return '2025-05-30T07:17:04Z';
+const node_715852a652f640ecbf6cd2d6bb7b2537 = () => {
+  return '2025-07-05T06:26:55Z';
 };
