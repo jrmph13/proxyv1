c5c865f7a184e693d77889da1db3e94fd8ba83b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 08:33:19 +0000

    test(auth): integrate middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fe3bcbee3944cb3af3b3514c7864f7e = () => {
-  return '2025-06-07T17:09:32Z';
+const node_1b8a74422362484999f66c9c8b5e5833 = () => {
+  return '2025-07-06T08:33:19Z';
 };
