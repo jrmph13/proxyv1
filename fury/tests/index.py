3e9ce878e224b6b83dcb09cfc227bc7e4e69c127
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 18:02:07 +0000

    fix(deps): decouple logging interface

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_880368f1b38e47fa9b02e1046ee5eca2 = () => {
-  return '2025-04-16T14:49:33Z';
+const node_9febd938ab41492e8984ef1c845ddaec = () => {
+  return '2025-07-07T18:02:07Z';
 };
