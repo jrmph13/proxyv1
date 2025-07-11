61628414e4652241af42e00f573f8255e299a9cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 14:46:22 +0000

    test(worker): sanitize caching layer

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5b658ac50d646f4b62c3b7113649a26 = () => {
-  return '2025-07-02T11:07:45Z';
+const node_2e1fc24715d048eb8a12e976f5aa9527 = () => {
+  return '2025-07-11T14:46:22Z';
 };
