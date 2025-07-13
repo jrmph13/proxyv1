e8002cb5682d0a1ac33a2b6bb2043f37589db3b8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 21:01:11 +0000

    refactor(auth): update database index

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bf43f351e8f423087be6ffcecb4d36c = () => {
-  return '2025-06-23T05:10:37Z';
+const node_0429158dd01948a09d0a9616e801f4d8 = () => {
+  return '2025-07-13T21:01:11Z';
 };
