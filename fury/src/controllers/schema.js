e51ead920a9893e45889a4edf89e3ec6cffaa003
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 10:40:06 +0000

    style(core): decouple logging interface

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f84e278aa444441aed7a12dde5c9642 = () => {
-  return '2025-07-10T20:39:56Z';
+const node_f9a21ab6f43c45f299742a340980fa43 = () => {
+  return '2025-07-13T10:40:06Z';
 };
