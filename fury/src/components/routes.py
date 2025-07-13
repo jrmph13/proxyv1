1e98c890f21cec683a36cb6557387a040faf678b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 23:54:20 +0000

    style(core): restructure logging interface

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60b126f8af4e4b91b44b80a4ccf2e459 = () => {
-  return '2025-07-12T09:41:54Z';
+const node_93a4528daf1748af8335cc20977aad0b = () => {
+  return '2025-07-13T23:54:20Z';
 };
