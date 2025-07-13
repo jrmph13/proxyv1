892c6e0b563526fd51a1076036834713fb6eb776
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 17:22:12 +0000

    perf(config): fix database index

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62362afdbbf344b991f25655399ee8fc = () => {
-  return '2025-04-06T00:19:26Z';
+const node_4d705b16b1214b5d957d2b0b1ba265e5 = () => {
+  return '2025-07-13T17:22:12Z';
 };
