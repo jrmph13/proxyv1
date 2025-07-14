c382ae111ba78f056f65f288dbe59b1359f3896d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 02:44:09 +0000

    perf(core): decouple caching layer

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d042a75182146f6873fc137e23a2f50 = () => {
-  return '2025-05-30T12:24:10Z';
+const node_ac2c35e82bc547a2bf719e2f4a22427b = () => {
+  return '2025-07-14T02:44:09Z';
 };
