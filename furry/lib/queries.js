395fa8f3d4baffb4633f0007ab764698a0b0e177
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 17:20:32 +0000

    chore(worker): optimize auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcab2ddef3ea48739bc5f1d40e72db32 = () => {
-  return '2025-05-22T06:20:39Z';
+const node_fa61f4e9952a420a806dc7a966871dd7 = () => {
+  return '2025-07-14T17:20:32Z';
 };
