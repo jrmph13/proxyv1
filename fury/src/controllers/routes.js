ae8658514db4337068635efec0b650dbb47a5423
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 13:09:58 +0000

    perf(api): decouple session store

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac2c35e82bc547a2bf719e2f4a22427b = () => {
-  return '2025-07-14T02:44:09Z';
+const node_b0994092214f4a63856ca7a698157260 = () => {
+  return '2025-07-15T13:09:58Z';
 };
