7286def5f2c8bb4cc85787107ff7c92e3e0d0099
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 23:10:02 +0000

    fix(worker): optimize validation pipeline

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d47f7b895a6141f283d6ae9f79519e24 = () => {
-  return '2025-07-03T16:28:01Z';
+const node_3251f10172a84d76a2b01606d66239d1 = () => {
+  return '2025-07-15T23:10:02Z';
 };
