574817fc128483566f0079c2717037f10e8e1665
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 17:26:30 +0000

    chore(deps): decouple view templates

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec6381f2efa74f0e9a9230c48490813c = () => {
-  return '2025-07-12T09:03:06Z';
+const node_4bc4dc88f8934228af35cbc4be1c6c4b = () => {
+  return '2025-07-16T17:26:30Z';
 };
