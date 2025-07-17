4fdf2e3fb99849dc77f30d9b5f19208aae257c1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 01:47:29 +0000

    feat(api): restructure auth tokens

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1b4b40b7ba44818953d546884703ae1 = () => {
-  return '2025-07-03T15:04:27Z';
+const node_b7e21b65020548a18601dd0f0eb995d6 = () => {
+  return '2025-07-17T01:47:29Z';
 };
