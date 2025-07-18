dae155400ae583bf40471466ea4d21214baacf6b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 16:18:20 +0000

    chore(deps): sanitize payload parser

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ccb367478af246128baaa361828ac5d4 = () => {
-  return '2025-07-17T15:15:20Z';
+const node_ee44843d2aca4cb1aaaaaed897ab3140 = () => {
+  return '2025-07-18T16:18:20Z';
 };
