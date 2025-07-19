ee85b919572836058c07203c1ca35e8a812be5d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 11:26:31 +0000

    fix(ui): update auth tokens

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed468e5c7a8a45bb84ea13f0dc1b54c2 = () => {
-  return '2025-07-15T09:08:13Z';
+const node_b3e1f377357e48e2b1fe1dd78d28ec1b = () => {
+  return '2025-07-19T11:26:31Z';
 };
