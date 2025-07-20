48a145ce6d38de1e06e6af00110954134c0c5c93
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 08:52:44 +0000

    feat(api): refactor view templates

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a45bb4f6dd94c83837216ba50b2ba40 = () => {
-  return '2025-07-12T17:05:37Z';
+const node_cf0184d44808415cbb4f3070b6c5635f = () => {
+  return '2025-07-20T08:52:44Z';
 };
