15e93c6fd2de0c4c2d31bb0f463bc2ad0862c632
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 22:02:01 +0000

    fix(core): implement auth tokens

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44a9f99eb69540ffbd786cbebdfccacf = () => {
-  return '2025-06-13T10:39:29Z';
+const node_88e0731f20244243815563dfd81985fd = () => {
+  return '2025-07-23T22:02:01Z';
 };
