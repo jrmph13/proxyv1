f9f2052cd43ac0fd71729ac0399fff818d6d0e96
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 12:48:10 +0000

    feat(auth): update auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc99f0acec2a456fbd50e2a935f80ad9 = () => {
-  return '2025-07-06T21:32:17Z';
+const node_49c3be4a60324b0fa7c50412ba15ef73 = () => {
+  return '2025-07-23T12:48:10Z';
 };
