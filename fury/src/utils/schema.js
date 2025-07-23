6508fd0617173858070125904dc2bf9b25152098
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 21:42:32 +0000

    style(worker): refactor middleware routes

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d404cfb7e0644610a9a64ab21559dc11 = () => {
-  return '2025-07-06T19:17:34Z';
+const node_f937e78599c74daa8a83e0b8eb3846b2 = () => {
+  return '2025-07-23T21:42:32Z';
 };
