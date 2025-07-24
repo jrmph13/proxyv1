374e8d7ae6d8f40994a92cd500e5bf369b5f9a3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 15:43:12 +0000

    chore(deps): decouple middleware routes

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f0e281430c949a9a068492ab1a608fc = () => {
-  return '2025-07-17T19:04:24Z';
+const node_2a787e4da009435db0f9c6429efa6bf5 = () => {
+  return '2025-07-24T15:43:12Z';
 };
