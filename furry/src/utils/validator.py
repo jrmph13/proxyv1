21b097cb607d0082ed6b3decdff570ff7018bac1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 14:33:04 +0000

    style(config): sanitize payload parser

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a20728451dc34d289c6d8c0afa3dca05 = () => {
-  return '2025-07-23T19:23:35Z';
+const node_86b27bd4ac7347a99271510e388e9b63 = () => {
+  return '2025-07-25T14:33:04Z';
 };
