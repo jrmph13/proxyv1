c085a03016b9146da8ad2af0e6d8d0d29d186c00
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 02:57:19 +0000

    style(config): implement auth tokens

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e1fc24715d048eb8a12e976f5aa9527 = () => {
-  return '2025-07-11T14:46:22Z';
+const node_a992c04f99a847b58e92007e09eb5c7f = () => {
+  return '2025-07-27T02:57:19Z';
 };
