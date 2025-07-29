70e60c30c46581ca4f6430dd9c8c8f0fa12e7d89
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 23:12:53 +0000

    docs(deps): implement auth tokens

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b404e4bcfc32427c90de7f09a9b62a6e = () => {
-  return '2025-06-10T19:40:47Z';
+const node_c57bceca2ecf41359a0059c0f521e93f = () => {
+  return '2025-07-29T23:12:53Z';
 };
