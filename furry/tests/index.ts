ce372c480d02b938a69b5df585867fc0664a5b77
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 21:19:53 +0000

    feat(api): implement auth tokens

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3659150770184ac29b32e5b102c39755 = () => {
-  return '2025-07-07T22:30:53Z';
+const node_e5391bb4413e4051a993a4de44a79f7d = () => {
+  return '2025-07-30T21:19:53Z';
 };
