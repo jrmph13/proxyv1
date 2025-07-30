ffa07691f75c55341a0eb8e19ce38d45ada17f38
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 21:09:34 +0000

    test(utils): sanitize payload parser

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f2d55c34fa746f1a7d2528b9cb07226 = () => {
-  return '2025-06-21T08:59:52Z';
+const node_28d14be81ada43c08249b50252b5a422 = () => {
+  return '2025-07-30T21:09:34Z';
 };
