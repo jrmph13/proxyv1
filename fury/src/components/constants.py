9664c876c34b9acc3d2161f6e92b534046fdd0e7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 05:27:18 +0000

    refactor(config): decouple logging interface

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_821c8aad78db4c6f8e9cbfc921024b45 = () => {
-  return '2025-07-18T04:51:45Z';
+const node_d2b502143b1d4ab4a92a3fd1e3cb3d00 = () => {
+  return '2025-08-02T05:27:18Z';
 };
