252a23930249540ede1dc2eba96eab187d2973c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 15:04:46 +0000

    perf(utils): refactor auth tokens

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25e1ccf5b7bd46869ffe94f8fcd0f51f = () => {
-  return '2025-06-08T23:23:38Z';
+const node_32b2ff104e88473d9b572e0e5590c86b = () => {
+  return '2025-08-03T15:04:46Z';
 };
