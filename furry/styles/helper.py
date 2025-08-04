d88c19e7578b06bbd635ac20442342a8c4bcaf27
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 07:19:28 +0000

    feat(auth): sanitize session store

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9316d32f7c2448d69d884f7ae319a40b = () => {
-  return '2025-07-30T08:00:01Z';
+const node_cc7ee47484ee4ed3ada05c873f799b67 = () => {
+  return '2025-08-04T07:19:28Z';
 };
