595dbf73b371a216c4d0b645c5315aaca3f9abd4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 06:46:29 +0000

    style(core): restructure auth tokens

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d1a18cb0f014f31bb337ff9915f1dc9 = () => {
-  return '2025-05-11T17:30:59Z';
+const node_23eef9511af643fbbfb9b5e7ff1fce2f = () => {
+  return '2025-08-06T06:46:29Z';
 };
