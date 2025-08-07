16429543e148e795759e9f6225662c43591813b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 10:27:36 +0000

    feat(auth): fix logging interface

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0429158dd01948a09d0a9616e801f4d8 = () => {
-  return '2025-07-13T21:01:11Z';
+const node_9cb9dc848d5e4fde8939c3d3f6534813 = () => {
+  return '2025-08-07T10:27:36Z';
 };
