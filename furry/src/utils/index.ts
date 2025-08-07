6756a8864724aa908a88671a935d45fbc64d5c8f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 23:26:20 +0000

    perf(core): enhance auth tokens

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_810feeec66c94ce494808a1be2879246 = () => {
-  return '2025-07-14T06:48:47Z';
+const node_4e75bdf007754b5c8e53c4335dcab0a8 = () => {
+  return '2025-08-07T23:26:20Z';
 };
