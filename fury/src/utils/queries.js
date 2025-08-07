cd11add1a9ad580a9ece5c3c4d4685f36c2ca1d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 11:52:01 +0000

    style(core): restructure payload parser

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2c5f6ed397041689878bab3b628d093 = () => {
-  return '2025-08-05T00:01:32Z';
+const node_a4392fc823c6410e9a44307d2f637e4e = () => {
+  return '2025-08-07T11:52:01Z';
 };
