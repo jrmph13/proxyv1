5d7176a1186f9b1d2b00c9516d02a14aad8652dd
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 20:33:13 +0000

    feat(db): update auth tokens

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1cf727480364745a7a9b121a08219a2 = () => {
-  return '2025-08-04T23:28:41Z';
+const node_a9d53c7430e04236a2aafc85008c2c07 = () => {
+  return '2025-08-08T20:33:13Z';
 };
