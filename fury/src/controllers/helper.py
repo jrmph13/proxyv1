667c1be590a25fea63e5ce370c267247b5fc0674
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 01:29:20 +0000

    feat(auth): integrate view templates

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4e47c7353744bd8b8729b865bf5b36e = () => {
-  return '2025-08-05T00:33:59Z';
+const node_e0e36b7359e84aecae5c3868df0e2a4f = () => {
+  return '2025-08-10T01:29:20Z';
 };
