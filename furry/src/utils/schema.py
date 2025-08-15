4b84ab55b7ef9987efc04673555dac18dd0f6748
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 16:49:43 +0000

    fix(core): optimize logging interface

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3324a312080a46c9aa01c0d626a4deb6 = () => {
-  return '2025-08-05T07:52:10Z';
+const node_ce2d4aa5670c4940af4eeeae73b7e928 = () => {
+  return '2025-08-15T16:49:43Z';
 };
