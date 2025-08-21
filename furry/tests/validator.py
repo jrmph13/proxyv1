a4a4881088b268f61fea89dfd5a1be477f7576a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 19:04:10 +0000

    chore(config): sanitize payload parser

--- furry/tests/validator.py
+++ furry/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3fc90d55c574476a8e5e842275eeb67 = () => {
-  return '2025-06-28T21:07:07Z';
+const node_632c74699fb247eb8fb6025fe8b4f247 = () => {
+  return '2025-08-21T19:04:10Z';
 };
