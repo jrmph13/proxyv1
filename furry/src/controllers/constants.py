fba06f6ed38993ea8336ab8a04c6920af5668ab1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 18:23:12 +0000

    style(utils): implement auth tokens

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31d70c81a8c34b9d928b1c5c22bfc300 = () => {
-  return '2025-07-15T19:16:04Z';
+const node_26ed143364bf47e1ad7e7fbb7c68873d = () => {
+  return '2025-08-30T18:23:12Z';
 };
