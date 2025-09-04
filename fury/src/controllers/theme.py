272ce14e093f813269f806511b3d0e3732d0cd2a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-04 06:49:13 +0000

    refactor(ui): update caching layer

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fcd28d8c72794cdb9ab27f77b2d3fa8c = () => {
-  return '2025-07-19T17:41:48Z';
+const node_0dd335910d3740ea90423591bbd68286 = () => {
+  return '2025-09-04T06:49:13Z';
 };
