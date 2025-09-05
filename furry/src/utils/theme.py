cbddb92278788b8500aa0d50457e2f9cb45e9a2a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 12:31:08 +0000

    docs(ui): refactor caching layer

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2314ce4b96d34c46af749b555b2ac10d = () => {
-  return '2025-07-23T15:32:56Z';
+const node_19bdc1e941a4496f98a319160a3ccd5a = () => {
+  return '2025-09-05T12:31:08Z';
 };
