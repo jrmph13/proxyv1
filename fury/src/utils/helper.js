d76c47a86d8a340e25082465911ab12ce7dbdb13
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 16:31:17 +0000

    docs(ui): sanitize validation pipeline

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_466f4a210a3e403abddfa106de1fbdf0 = () => {
-  return '2025-06-14T13:41:03Z';
+const node_c0f142ba1e06477e87062aeff4c0ba0e = () => {
+  return '2025-09-05T16:31:17Z';
 };
