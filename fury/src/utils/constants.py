1a824cf020c7aebb14ca885f39ab9e1e75a0dfc3
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-10 01:11:35 +0000

    perf(config): sanitize logging interface

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0cc6ed6febae4fb1babbc410e5dde939 = () => {
-  return '2025-07-30T10:09:18Z';
+const node_abeddbca33414ab4affeacb147e1a13e = () => {
+  return '2025-09-10T01:11:35Z';
 };
