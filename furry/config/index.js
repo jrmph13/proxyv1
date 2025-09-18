8d38cca63a8dffa9eef0f9198ebce1dd28548d8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 06:02:06 +0000

    style(api): update middleware routes

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43106aba40f54b30be5657b26cd06ae6 = () => {
-  return '2025-08-10T19:28:13Z';
+const node_e5c617afa7374c20a0ada148e7576fe6 = () => {
+  return '2025-09-18T06:02:06Z';
 };
