e0e8f195c998e59f9f384114406c8f900ca323b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-19 06:45:52 +0000

    chore(api): restructure middleware routes

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46bf4eeb059a4ef799973137ac7f5e1c = () => {
-  return '2025-09-15T01:51:00Z';
+const node_46627e51c80143c3be9d2c259eb520aa = () => {
+  return '2025-09-19T06:45:52Z';
 };
