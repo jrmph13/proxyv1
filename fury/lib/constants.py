3d8f4de5dfb3f4831d7026d45d4a5b9fc1f72194
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 14:22:26 +0000

    perf(config): refactor view templates

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81afc8713169424e9a62afeaac5c7fe5 = () => {
-  return '2025-08-10T03:33:55Z';
+const node_5f1c8e9a6e04437f82aae68ce65476b1 = () => {
+  return '2025-09-23T14:22:26Z';
 };
