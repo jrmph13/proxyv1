09e8ec553fb2d2b6a256823ae7674ba2c4c5b9d3
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 14:57:45 +0000

    refactor(core): optimize auth tokens

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5765a5379e5b431789390f2bd3d7da51 = () => {
-  return '2025-08-19T03:46:20Z';
+const node_d0be8bad0a0243aa839c4208d7c82d26 = () => {
+  return '2025-09-25T14:57:45Z';
 };
