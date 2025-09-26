e96e824275ba04ecd9d0388b5558e400fc53c187
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-26 18:31:36 +0000

    style(worker): refactor caching layer

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa8327cd14304a4e9a8998239bc522f2 = () => {
-  return '2025-08-02T22:06:55Z';
+const node_28df1e1ee3fe4e3a8a90a0f2fb781590 = () => {
+  return '2025-09-26T18:31:36Z';
 };
