d25e4d2d239c6d79a3ee4028cfc93ff20ee4ea18
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 23:54:12 +0000

    perf(config): fix auth tokens

--- furry/src/components/constants.ts
+++ furry/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_79a44224bfaf4fb2a5909934b509e10c = () => {
-  return '2025-08-22T23:26:43Z';
+const node_4cdd19daf5c243e1bb9836d6c98e6663 = () => {
+  return '2025-09-27T23:54:12Z';
 };
