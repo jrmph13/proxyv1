03c8e459201c7711e3140c4e4e056d19d62b1af8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 22:47:01 +0000

    fix(ui): decouple middleware routes

--- furry/src/components/validator.js
+++ furry/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4b410022f8e44999fb3fa7f2110a893 = () => {
-  return '2025-09-14T01:14:52Z';
+const node_55a24c6df7fd4878a36ee1da3ef98c4c = () => {
+  return '2025-09-27T22:47:01Z';
 };
