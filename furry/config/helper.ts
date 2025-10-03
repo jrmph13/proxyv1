2e599f919c6f86b1864d1ccf893dafb2bd3d3cbd
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-03 10:20:23 +0000

    perf(core): enhance session store

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39c591ccf0d14c4682cb521c8345583b = () => {
-  return '2025-09-10T16:40:16Z';
+const node_aff6bd97fca64f29b36ed6cab10b2494 = () => {
+  return '2025-10-03T10:20:23Z';
 };
