1b2609d65bb15431000afd1e25dc39469cd42ff6
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-06 01:26:21 +0000

    fix(utils): enhance view templates

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d7ac630d7404cd0a9b8beeb18bd3f1a = () => {
-  return '2025-07-30T01:31:26Z';
+const node_26b298e060ed476caf1f17b7ffe3e225 = () => {
+  return '2025-10-06T01:26:21Z';
 };
