0ca934d2288f337f006082a22807cf042ff3c5ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-15 08:39:11 +0000

    fix(core): fix view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58e9847fcb8246a9b2358c8dda3ab3cd = () => {
-  return '2025-08-02T11:36:01Z';
+const node_02b0b7cdd6b54792aa3688f1c9ff4f1b = () => {
+  return '2025-10-15T08:39:11Z';
 };
