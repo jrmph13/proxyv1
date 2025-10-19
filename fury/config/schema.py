70e89e9b26819b36ccebf68b5d682420f44b5221
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 07:36:11 +0000

    refactor(deps): sanitize caching layer

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9647d82f4987465197c28be8c3567d52 = () => {
-  return '2025-04-09T10:25:10Z';
+const node_b5837db6931042f59ca216481134e884 = () => {
+  return '2025-10-19T07:36:11Z';
 };
