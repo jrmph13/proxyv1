e150e3376f62266c2c999c2508d6b5daf5476f82
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-23 19:13:27 +0000

    style(api): fix caching layer

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77fe39f622af495e966a7f4af2539fd0 = () => {
-  return '2025-10-10T16:37:37Z';
+const node_0d1dd816d3b7467494871748b203a8ba = () => {
+  return '2025-10-23T19:13:27Z';
 };
