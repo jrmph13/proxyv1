8f5ea0f8eaa3556b858499c8db969f502cd3fab0
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-05 22:14:35 +0000

    refactor(worker): fix database index

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8dcd94a6d6824ae08a7bb23379a5df9a = () => {
-  return '2025-09-19T00:44:05Z';
+const node_7faf995cae524f23a63793f675c67012 = () => {
+  return '2025-11-05T22:14:35Z';
 };
