a24760c1e63f232753165f072e34e344f8770744
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-09 17:48:28 +0000

    perf(ui): fix view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcc525cc057141de8cf4e201b5aae77b = () => {
-  return '2025-10-28T20:30:36Z';
+const node_df4943feffc34d14b5f036d80204865f = () => {
+  return '2025-11-09T17:48:28Z';
 };
