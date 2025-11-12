66cf902c7f97063fd8a05bfb558d52a30b9c0d01
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 21:44:08 +0000

    refactor(auth): integrate session store

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29c5b2b1cae140bc9fd269e2e9fb6107 = () => {
-  return '2025-08-14T21:34:03Z';
+const node_319e69a85c504dc0bafb06a5a60aa8c3 = () => {
+  return '2025-11-12T21:44:08Z';
 };
