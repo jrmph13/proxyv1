2ddaef225d42cbce418181a107a1ee8f8ba6ae5d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 06:47:24 +0000

    chore(core): integrate view templates

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c74110b64a00458b805505581eede1e6 = () => {
-  return '2025-07-31T16:52:28Z';
+const node_f126d46ecab0420e9cc80993c6f8bd93 = () => {
+  return '2025-11-14T06:47:24Z';
 };
