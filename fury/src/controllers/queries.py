bd993cba54751879a65260ac9c84ea8869c8730d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-21 20:13:46 +0000

    refactor(deps): fix logging interface

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5aee223c8a2487ea8dbcbb556490d9b = () => {
-  return '2025-07-16T03:06:17Z';
+const node_38d83008e56f4bd3afdb7cc677abacd7 = () => {
+  return '2025-11-21T20:13:46Z';
 };
