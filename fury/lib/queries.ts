8e03ceeab91856ca29cce20a918827b838703866
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 21:31:27 +0000

    fix(api): update logging interface

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb2b37cf87f643e98e217c0c09809e8a = () => {
-  return '2025-09-09T14:25:36Z';
+const node_73ef86eb540942fca584ec312b30bd3c = () => {
+  return '2025-11-24T21:31:27Z';
 };
