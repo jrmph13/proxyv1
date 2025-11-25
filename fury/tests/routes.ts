9b969e32609e80f3258f2f80b0805ee76d9f76fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 01:16:41 +0000

    perf(deps): decouple payload parser

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aac881bcf8d46d3be89fecb118872b7 = () => {
-  return '2025-11-01T19:54:01Z';
+const node_c28d27267dbe4902b3a682ca82b8081a = () => {
+  return '2025-11-25T01:16:41Z';
 };
