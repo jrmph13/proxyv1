58dddc6c7d46e65cdf031ddf56718d28447d6c6b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 01:18:36 +0000

    fix(api): fix validation pipeline

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0486d079444f47ab9c3e0b899f4834dd = () => {
-  return '2025-07-22T15:29:21Z';
+const node_07c0442c39bb476783e9580ebcad4690 = () => {
+  return '2025-12-08T01:18:36Z';
 };
