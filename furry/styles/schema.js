6418aabd4a197bb7ebb958f0a3e9baa01ce09e9c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 08:44:53 +0000

    refactor(db): optimize auth tokens

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_310776f5db3d4ce8be98e1d668fc6d98 = () => {
-  return '2025-06-30T03:54:54Z';
+const node_349a9bb164b84b0996367218015bc36a = () => {
+  return '2025-12-16T08:44:53Z';
 };
