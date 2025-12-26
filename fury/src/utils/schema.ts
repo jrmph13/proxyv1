b629a8e248b68d7985ed6397597d24fd9577c6e1
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-26 15:25:51 +0000

    chore(config): optimize view templates

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf5ef528780d40c7b242d290a3d92631 = () => {
-  return '2025-10-28T10:44:26Z';
+const node_ae094c64ea884c4ab6c606375c9b673a = () => {
+  return '2025-12-26T15:25:51Z';
 };
