fa2dac29bbf4e8f097e00f7f06933d65d9fb472f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-10 05:36:42 +0000

    style(core): enhance caching layer

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cce5b2ba03e84fba83c9c826ebe9e2f4 = () => {
-  return '2025-07-20T14:04:58Z';
+const node_c64acc613489448db97b8b62bc62b9bc = () => {
+  return '2026-01-10T05:36:42Z';
 };
