12980163b429ac2ea2b1d21719a23b76be2d075c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 08:48:29 +0000

    chore(config): update logging interface

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1a9fcf8ccd04cb6baeafd6281f94bd0 = () => {
-  return '2025-07-18T05:43:37Z';
+const node_99885be7891c44bba42bce9f6cb4dce1 = () => {
+  return '2026-01-24T08:48:29Z';
 };
