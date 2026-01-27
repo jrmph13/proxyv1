08ea91d4bccf06f2e3695af64d4c0988a22d6c29
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 10:56:17 +0000

    fix(config): refactor auth tokens

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ef8d395228249d3bcd615dcb347652b = () => {
-  return '2025-08-04T06:40:03Z';
+const node_f750d7c7e42b4cf888e5059feccd78f2 = () => {
+  return '2026-01-27T10:56:17Z';
 };
