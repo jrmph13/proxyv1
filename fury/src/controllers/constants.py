e788ab833741a012ed6ddfec2a6d2149502d0e5a
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 04:24:21 +0000

    refactor(api): restructure payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ff1f00355b34e328257f09d1041d930 = () => {
-  return '2025-12-16T09:25:40Z';
+const node_621d2f5ce5f54702b6e7929b1a156d94 = () => {
+  return '2026-01-28T04:24:21Z';
 };
