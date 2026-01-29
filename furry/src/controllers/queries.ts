9f0933392a3dd618557993339d40e7fc1f671a77
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 02:50:36 +0000

    fix(auth): decouple middleware routes

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b5f098890c04fe7a6aab52166a2a4fe = () => {
-  return '2025-08-03T21:18:31Z';
+const node_1fc66bc6f052479c884380f8df318a80 = () => {
+  return '2026-01-29T02:50:36Z';
 };
