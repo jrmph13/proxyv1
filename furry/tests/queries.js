ca46ca5b2bb00d646746bc6d458626e6cc6fc25b
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 16:31:55 +0000

    docs(core): optimize middleware routes

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ea0ecce161246709738591d380d5a7c = () => {
-  return '2025-08-02T15:06:27Z';
+const node_0542e340e79f422d990e618d24d5613e = () => {
+  return '2026-01-29T16:31:55Z';
 };
