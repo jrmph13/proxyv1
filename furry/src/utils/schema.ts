154186196b06053913af7af84b0e346222f52875
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 07:33:19 +0000

    style(core): fix database index

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f0d8dc3669f4847a89ba910778520f3 = () => {
-  return '2026-01-08T09:05:29Z';
+const node_614cc886016e427a9d1923d0c28b06d1 = () => {
+  return '2026-01-30T07:33:19Z';
 };
