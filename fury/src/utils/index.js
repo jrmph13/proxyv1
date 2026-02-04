f886db3c3b010b253a83d445fe83001bbecdbf39
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 08:52:09 +0000

    style(config): restructure auth tokens

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37c441a700f749c6b8a4501fceba7e86 = () => {
-  return '2025-12-08T10:34:44Z';
+const node_01b5b38bd1954122822d37b3f956fd1c = () => {
+  return '2026-02-04T08:52:09Z';
 };
