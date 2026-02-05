a3071702cb112d3d5a3ec8a2b9cb2bf1392153f8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 11:01:22 +0000

    style(auth): sanitize auth tokens

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fe18170ef1f41f1b510f9ecd73da9c8 = () => {
-  return '2025-09-09T16:20:04Z';
+const node_1d9f4d0467604005bb32ca3525eca93e = () => {
+  return '2026-02-05T11:01:22Z';
 };
