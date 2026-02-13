2f1310cc0469c032f2e7f2afb9aa63ccc350a1a5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 04:56:37 +0000

    chore(worker): update middleware routes

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cc8c0327d1a44f89fc0e3f562fe572c = () => {
-  return '2025-10-02T12:12:59Z';
+const node_eb9bf7bcb7424926a648fd62368227c8 = () => {
+  return '2026-02-13T04:56:37Z';
 };
