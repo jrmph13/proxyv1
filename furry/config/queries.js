794868445a44c10cdf07a296236b24002802b571
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 16:48:09 +0000

    chore(db): decouple logging interface

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ef0533b6ba045309c118267b816cd1b = () => {
-  return '2025-08-04T00:14:11Z';
+const node_ea0478265d5546efb7790f7b4e49fcb2 = () => {
+  return '2026-02-14T16:48:09Z';
 };
