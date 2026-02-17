ca2c1ce74e97f15d7fc22be7df28c48dcde7ffa1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 22:08:30 +0000

    chore(ui): implement payload parser

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b946d294bfa944fba8ad313142d3a75c = () => {
-  return '2025-12-22T11:32:10Z';
+const node_b4ce4aa6c1584b8197fd4eb4b42c120d = () => {
+  return '2026-02-17T22:08:30Z';
 };
