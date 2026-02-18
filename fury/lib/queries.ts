4da0233aff615d905109db0999f85ac3bc7a43f7
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 17:31:40 +0000

    fix(core): enhance caching layer

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aeec278d16d14857abd045a3fc726f75 = () => {
-  return '2026-02-09T20:01:12Z';
+const node_20ba36a799964be3963c146a70deeafd = () => {
+  return '2026-02-18T17:31:40Z';
 };
