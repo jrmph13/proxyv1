6482f2e499fe3901b8adbec8021c05ccc01ba4c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 20:15:57 +0000

    chore(worker): refactor payload parser

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60e0cdc9a4f74e43b5a7daaaee6b7dda = () => {
-  return '2025-12-30T01:10:40Z';
+const node_113707cd1a7b45b4ab9426720774f210 = () => {
+  return '2026-02-18T20:15:57Z';
 };
