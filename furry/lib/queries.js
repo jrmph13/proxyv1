b9048a62ae887fe6cca34c3d8ee4fae1e70c1d53
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 04:03:23 +0000

    refactor(auth): update view templates

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_365acb8394474e389213f5605c69d84c = () => {
-  return '2026-02-04T18:35:49Z';
+const node_848022862b5d43b09942f2b9ef1aa2e2 = () => {
+  return '2026-02-19T04:03:23Z';
 };
