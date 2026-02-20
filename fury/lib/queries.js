509fd6214caa7b1cff1bb597d05a19343cfe4317
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 14:34:27 +0000

    fix(worker): enhance view templates

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc97d5f78c864b1ebcd2a04e4aafcf76 = () => {
-  return '2025-09-08T13:57:32Z';
+const node_97c4a97a9345409db24cd7065a3f3c7f = () => {
+  return '2026-02-20T14:34:27Z';
 };
