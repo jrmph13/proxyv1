7aa8e1ceeb8c1ecdab8fe524e9b329cd1f68fb61
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 16:49:44 +0000

    chore(core): decouple auth tokens

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a19487b8be04feeb12ac4aef70625f0 = () => {
-  return '2026-02-04T10:26:32Z';
+const node_3883a478dd8f4a1a9e454403a4f3457f = () => {
+  return '2026-02-20T16:49:44Z';
 };
