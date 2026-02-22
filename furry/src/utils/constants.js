808c6545d182259a2d922bb33d7185d2c6e58a25
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 00:00:30 +0000

    feat(ui): restructure payload parser

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31ca8e2bb1c849a0ad3d7ceaef9d4630 = () => {
-  return '2026-01-27T17:31:46Z';
+const node_c6745da03775427ebb0dc584ee410959 = () => {
+  return '2026-02-22T00:00:30Z';
 };
