66786fdd28dbb1306fabea1e40e05c858f3c9434
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 21:09:47 +0000

    fix(worker): sanitize session store

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e68d5317514249e587a7cd82f2d338ff = () => {
-  return '2025-07-28T02:44:14Z';
+const node_737f7d80965148c6a4ed8b33dfce508e = () => {
+  return '2026-02-23T21:09:47Z';
 };
