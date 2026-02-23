d312a3eb790a266ea4296c44023ba2af86a49093
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 19:44:04 +0000

    fix(api): enhance auth tokens

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6eee5ef4e8e54715b32e60d112fe3877 = () => {
-  return '2026-02-14T23:23:33Z';
+const node_dc2e4107372d48d6a9f3bea7c2004b9a = () => {
+  return '2026-02-23T19:44:04Z';
 };
