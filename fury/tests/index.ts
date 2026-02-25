32134eb419b9d4994706ffcd2b6913ecb650b43a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 12:19:54 +0000

    style(worker): fix caching layer

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5a442aa0aca41fb8ef76d872fb89d20 = () => {
-  return '2026-02-10T11:07:03Z';
+const node_15a1cbe6fcda46ffbb74220b9d7c3331 = () => {
+  return '2026-02-25T12:19:54Z';
 };
