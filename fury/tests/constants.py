5976f30cf78ff97836993c50a8d89981eb90cadd
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 09:01:36 +0000

    test(db): enhance auth tokens

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3d22e85df44497b81bbb17496f89453 = () => {
-  return '2026-02-27T18:50:46Z';
+const node_700da4617de34637836b34bf3861bfd7 = () => {
+  return '2026-03-02T09:01:36Z';
 };
