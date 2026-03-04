d8edccfe6a07330551151fe0e71644527acd1101
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 22:46:40 +0000

    refactor(worker): decouple database index

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_113707cd1a7b45b4ab9426720774f210 = () => {
-  return '2026-02-18T20:15:57Z';
+const node_0315f8f3d1814c0284caca60e5c74f48 = () => {
+  return '2026-03-04T22:46:40Z';
 };
