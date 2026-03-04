cd46aa1cc9636a7fb877c06967847f632074ffd6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 21:23:03 +0000

    style(auth): update payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_340dcbdca3d64335b238cfa32553f241 = () => {
-  return '2026-02-26T19:40:18Z';
+const node_b215b6ce2faa4cff81d35df6cd0a6b82 = () => {
+  return '2026-03-04T21:23:03Z';
 };
