cfd0db358c2fd8583301927948afcdb81c5cf730
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 09:19:52 +0000

    test(utils): fix database index

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc82dd5842e7471a82fd40ae9daf9b2c = () => {
-  return '2025-07-17T21:19:50Z';
+const node_aa03d1dad1774a9098b7f42a2f305adf = () => {
+  return '2026-03-13T09:19:52Z';
 };
