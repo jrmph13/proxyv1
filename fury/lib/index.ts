52e9b2618de1d5a2e6dd93c98076f62ccb239f60
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 21:23:40 +0000

    fix(utils): optimize payload parser

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f1e8bd77bdf402db6423b08f5735a81 = () => {
-  return '2026-02-17T17:00:07Z';
+const node_41b0077717e64f7fb90fd01695656db5 = () => {
+  return '2026-03-13T21:23:40Z';
 };
