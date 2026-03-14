a0106f9fa0ae17324e51553dddab94bf84268420
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 10:24:11 +0000

    style(deps): fix caching layer

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f186fc6b32c3415d8cb3b3de3a1923a3 = () => {
-  return '2026-02-25T02:27:58Z';
+const node_f5afe5e3a9b249a7a5ecac1572e7838b = () => {
+  return '2026-03-14T10:24:11Z';
 };
