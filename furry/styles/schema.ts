d564e3b4cbd81380e408038e8b3fe7e976ac8159
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 18:58:48 +0000

    chore(ui): restructure database index

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1dac3cb19da94235888de3ca457dfee5 = () => {
-  return '2026-02-23T23:07:17Z';
+const node_9eeab81fd77f4ad9a4bffacde6f165d6 = () => {
+  return '2026-03-16T18:58:48Z';
 };
