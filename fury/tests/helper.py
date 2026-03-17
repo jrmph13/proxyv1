3db5be1ae5b1c943d651545bcdc758e74c2e4947
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 03:37:19 +0000

    refactor(worker): sanitize middleware routes

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b25c95a3e9cc4ea0899320a5d0489428 = () => {
-  return '2026-03-08T22:33:58Z';
+const node_e7f0494c333d4c308c196f8fce884b07 = () => {
+  return '2026-03-17T03:37:19Z';
 };
