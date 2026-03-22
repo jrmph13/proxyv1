92891c13e21e453fccc65b55cb35be053134045f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 19:29:17 +0000

    docs(db): restructure middleware routes

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db5d6bb97cac48a2ad2de3a5d61bf337 = () => {
-  return '2026-03-15T01:10:33Z';
+const node_8740287c6cfa4edc83a92b5f8d3f698c = () => {
+  return '2026-03-22T19:29:17Z';
 };
