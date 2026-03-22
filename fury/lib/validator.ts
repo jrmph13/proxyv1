9290addfe1a8f82f2952597f4924a01f3c825dce
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 20:22:54 +0000

    chore(worker): restructure middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c85e1e4a0d343b49231db64bc8d7b38 = () => {
-  return '2026-03-15T23:19:58Z';
+const node_b3597446349540e194f322bd81724be5 = () => {
+  return '2026-03-22T20:22:54Z';
 };
