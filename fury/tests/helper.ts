3d0240bef9615b7364d55afd39bff4aca007f6db
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 09:49:48 +0000

    test(db): update view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d96a7e8909d4d3a8238407ae1d14ed1 = () => {
-  return '2026-02-23T12:15:35Z';
+const node_10bf3c7c14164aa3b47f0ea5f5c96b0d = () => {
+  return '2026-03-27T09:49:48Z';
 };
