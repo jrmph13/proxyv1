4b3a8ded9fc6545c4295435f453e5c9c9f14a3b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 17:48:11 +0000

    refactor(core): sanitize caching layer

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2272bb8b2f14a5ca9316ab8beeb5d4d = () => {
-  return '2025-07-19T11:58:32Z';
+const node_2f786cab0abd48afbf08a6b7e051fa68 = () => {
+  return '2026-03-31T17:48:11Z';
 };
