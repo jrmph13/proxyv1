062ca984272075bd4fd73e12adfee029cf864d84
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 19:04:18 +0000

    chore(core): sanitize payload parser

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f19b63c967bb42daa2c03a7633b6e721 = () => {
-  return '2025-08-25T07:50:07Z';
+const node_f0283902b5404097b852583f8178d16e = () => {
+  return '2026-03-31T19:04:18Z';
 };
