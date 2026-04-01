e8ee47708c4fc650abeab76106b6fea97554d415
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 23:26:31 +0000

    refactor(core): decouple caching layer

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b38ce172d3043afae7508f7cbdaeee5 = () => {
-  return '2026-04-01T07:55:16Z';
+const node_9964e705dd57472698f742384192d0da = () => {
+  return '2026-04-01T23:26:31Z';
 };
