86efe73840884ff0ef839a443b3f8712ab0fa508
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 02:19:07 +0000

    perf(utils): decouple payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b215b6ce2faa4cff81d35df6cd0a6b82 = () => {
-  return '2026-03-04T21:23:03Z';
+const node_8bb43c1e7bde4cd4aba17afd7b2c3c9b = () => {
+  return '2026-04-01T02:19:07Z';
 };
