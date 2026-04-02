4f524f0aa5a0384004f95fbc3de61f32362bbf60
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 05:11:09 +0000

    refactor(ui): restructure session store

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1db9596b8d44198a832a7daad3d599e = () => {
-  return '2026-03-05T21:34:49Z';
+const node_f2f729cddc0349c28865b1c704e22ed0 = () => {
+  return '2026-04-02T05:11:09Z';
 };
