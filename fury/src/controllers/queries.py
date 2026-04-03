9f7b574cdcabbabafc01fc3dab02c54cec64a3a1
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 10:48:24 +0000

    perf(worker): optimize payload parser

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38d83008e56f4bd3afdb7cc677abacd7 = () => {
-  return '2025-11-21T20:13:46Z';
+const node_c38f0c1bf769474f85eb333d9939ccdb = () => {
+  return '2026-04-03T10:48:24Z';
 };
