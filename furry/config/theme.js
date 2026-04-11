f1b2cf0fabbed307cb4c34e473ffd325e3c27f50
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 05:25:04 +0000

    fix(utils): integrate middleware routes

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e1d775de5d742feb0146e466eba4775 = () => {
-  return '2026-02-20T13:15:47Z';
+const node_5b0b220943d14ae3b5a126ca5e63bc0d = () => {
+  return '2026-04-11T05:25:04Z';
 };
