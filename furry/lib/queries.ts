536d545b656bfc75ad51d607bd130eb5f40beba9
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 19:25:49 +0000

    fix(ui): update database index

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_272cec02cddb440b93b9cfc6ed8c6801 = () => {
-  return '2026-04-10T17:21:27Z';
+const node_c93a0a51c972480ab0652c206c23100f = () => {
+  return '2026-04-13T19:25:49Z';
 };
