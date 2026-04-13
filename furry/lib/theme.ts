729538a538e46a084fd067a1722529d43785f115
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 15:51:49 +0000

    feat(auth): restructure database index

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_886afe8223ad4e959fdd117d4c969023 = () => {
-  return '2026-02-22T13:42:37Z';
+const node_19dd9c318e0a4da684948664befa9406 = () => {
+  return '2026-04-13T15:51:49Z';
 };
