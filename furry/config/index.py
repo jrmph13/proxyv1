cbf1d38fcb4ba6249ab57c6c8204f90f24fc4352
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 05:44:18 +0000

    docs(config): fix auth tokens

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9d9a376b27441a9b4aac0b2495630ee = () => {
-  return '2026-04-14T04:19:06Z';
+const node_27cb481137e54dbe970da2b67a96ee47 = () => {
+  return '2026-04-14T05:44:18Z';
 };
