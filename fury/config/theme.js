1299f10c9ceccd1785edb3b2cfff913daae429d4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 17:44:35 +0000

    fix(auth): enhance view templates

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1ea0a9b9f7344a1b1a4cfe3459bde50 = () => {
-  return '2026-04-03T02:39:18Z';
+const node_a82a1cc5a6f24509ac1d4101a84f6d8c = () => {
+  return '2026-04-15T17:44:35Z';
 };
