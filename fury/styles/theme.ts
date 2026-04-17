a1e0cc31bd4222afbb4c9917e0968c50ce11c45e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 21:45:58 +0000

    fix(core): sanitize session store

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a742d52f5b9346e1943bdcfa2cfdc2e6 = () => {
-  return '2026-02-18T17:27:13Z';
+const node_6d4ee585a489439e8e9cf984bd39b8b0 = () => {
+  return '2026-04-17T21:45:58Z';
 };
