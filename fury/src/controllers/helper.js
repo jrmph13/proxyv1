e846ec089642750807a0066a1fcb3dcd3a57c119
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 04:46:30 +0000

    style(utils): enhance payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_569392faebb240269b018db051ca2e2d = () => {
-  return '2025-12-08T03:45:40Z';
+const node_434f27e1b1434207b9a71e34c9bb9606 = () => {
+  return '2026-04-21T04:46:30Z';
 };
