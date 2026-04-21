de84d47a24c5678a5b427c74821a3d344c701af8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 12:44:08 +0000

    chore(utils): decouple middleware routes

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5be1e6b501f471897bdfd345435c95c = () => {
-  return '2025-12-11T21:02:46Z';
+const node_ac6bd27a05b0497b913c3d48d07f4add = () => {
+  return '2026-04-21T12:44:08Z';
 };
