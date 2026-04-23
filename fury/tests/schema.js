daa9010f58f620c917636d33ef482e3aa7d9a096
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 05:32:23 +0000

    fix(config): refactor view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c123eb497b348af808a736840289ac9 = () => {
-  return '2026-04-03T18:07:31Z';
+const node_7710b313777d46ec80f8720f3257dddf = () => {
+  return '2026-04-23T05:32:23Z';
 };
