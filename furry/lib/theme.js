5275a9a5ad0f58e2241b473ca4b0fe66e972bff7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 10:17:05 +0000

    feat(config): sanitize payload parser

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72d85479b5b74ea88b23c087485abed0 = () => {
-  return '2026-04-01T08:30:23Z';
+const node_23aaaab0ada54b3ea891a5b427f349cc = () => {
+  return '2026-04-24T10:17:05Z';
 };
