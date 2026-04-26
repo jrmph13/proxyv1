cf629ac9330d3a3bc82030f87fbfb09bdcca75da
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 01:05:24 +0000

    perf(worker): sanitize session store

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b9ef81053714e1dabbad4f7aa2b5e3f = () => {
-  return '2026-04-13T09:11:26Z';
+const node_f29222b291044165913dc5bc004face2 = () => {
+  return '2026-04-26T01:05:24Z';
 };
