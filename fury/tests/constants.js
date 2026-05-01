f25f9b69a568d264ae24b9365ac1b3554efb84e9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 01:18:46 +0000

    chore(db): refactor view templates

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6c433061503461196f36f1be7ca1deb = () => {
-  return '2026-02-16T16:57:36Z';
+const node_c400880a90f64f0f8384a30d0e7a448f = () => {
+  return '2026-05-01T01:18:46Z';
 };
