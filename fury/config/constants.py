72ee9dfae7ade6c52705547c65f9c830434fc74e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 05:55:18 +0000

    refactor(auth): optimize payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a8e8170a809486cab4f51a5f3b29443 = () => {
-  return '2026-03-18T18:12:22Z';
+const node_6bd4c3570b8848cfbdf97a9ca7095a79 = () => {
+  return '2026-05-07T05:55:18Z';
 };
