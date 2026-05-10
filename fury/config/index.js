e1ba414ad83c53c78d00654626c72cf1b8e8e578
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 14:52:03 +0000

    docs(core): refactor middleware routes

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50a148eacf754c2aa32470169ba96bc7 = () => {
-  return '2026-04-16T03:48:00Z';
+const node_4bd5815f363e4a13aaa63dc47d0c8d6a = () => {
+  return '2026-05-10T14:52:03Z';
 };
