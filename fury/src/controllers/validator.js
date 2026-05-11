80e7f300fb379f27de6378e91ba58c1888d6a5ef
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 00:06:06 +0000

    style(config): integrate payload parser

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0345cddc53ca44dfa45c975f4f52e328 = () => {
-  return '2026-05-05T15:59:26Z';
+const node_482639487c374e76a09fd50156ad851a = () => {
+  return '2026-05-11T00:06:06Z';
 };
