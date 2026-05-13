2e23d94526d3cb5b78decd378d1aa41f208b2cb6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 06:07:57 +0000

    docs(utils): fix logging interface

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98fe00fdd37e4b94a2c463e0b33a1126 = () => {
-  return '2026-03-13T06:41:57Z';
+const node_8f9cb6cf06f74af3ae0e432d2a0a3375 = () => {
+  return '2026-05-13T06:07:57Z';
 };
