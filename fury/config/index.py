9c90bce76307b6d5a0e47cd46cd257a4a3a33ed3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 12:46:00 +0000

    test(ui): decouple auth tokens

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99c3d7d96aee435ea5355d884ae45600 = () => {
-  return '2026-04-01T20:33:42Z';
+const node_7e34d36415884a19b65aceb37ed74562 = () => {
+  return '2026-05-18T12:46:00Z';
 };
