a73e3417bc16955e992a57c6e68f2a6aec7d850f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 20:40:08 +0000

    test(ui): fix auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80eb0503ab7547199755f9f65e7ca770 = () => {
-  return '2026-02-09T09:21:04Z';
+const node_c62f10a55b0d47ddadbc67f3b2853530 = () => {
+  return '2026-05-19T20:40:08Z';
 };
