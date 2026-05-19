6a51f2b5be51cb9efcb2742c82e987fdf6c11182
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 19:18:14 +0000

    feat(worker): optimize middleware routes

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eac5e307dd2e4d20a9a2ef9d3e310bc2 = () => {
-  return '2026-04-17T08:16:17Z';
+const node_1b7443f8273243e8a9cbe30e8b4c2b43 = () => {
+  return '2026-05-19T19:18:14Z';
 };
