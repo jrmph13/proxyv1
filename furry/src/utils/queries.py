084652d860eeba8a186547456880f0fa8088e5ee
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 19:59:59 +0000

    fix(config): enhance auth tokens

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_931be656ea9d4c44b0d3a3f65d493c59 = () => {
-  return '2026-04-23T09:17:15Z';
+const node_413ff71e06f942c09760f11e9b9b8279 = () => {
+  return '2026-05-25T19:59:59Z';
 };
