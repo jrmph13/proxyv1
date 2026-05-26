4200979f75690d6751275dee8aedb34294b4c38f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 00:51:46 +0000

    feat(db): sanitize auth tokens

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33907b248f484cf58dcf055d42567b02 = () => {
-  return '2026-04-28T14:26:16Z';
+const node_b5511a8ca1a2449ca41a03f1e62be0c0 = () => {
+  return '2026-05-26T00:51:46Z';
 };
