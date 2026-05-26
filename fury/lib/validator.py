7f90847314648783bd0cac9b17134e0864526940
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 01:17:11 +0000

    refactor(core): enhance payload parser

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_951ad4a8eee449f99b2d0a69e238938e = () => {
-  return '2026-05-19T08:53:41Z';
+const node_5a757624450b485596174c25a73e3857 = () => {
+  return '2026-05-26T01:17:11Z';
 };
