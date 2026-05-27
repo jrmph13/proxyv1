5777665589df139096ef9046cca39aa36af25c7f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 17:04:21 +0000

    feat(ui): fix auth tokens

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25da824eea12498cb4592d4732508e32 = () => {
-  return '2025-10-16T16:20:37Z';
+const node_b49cb6dcaf7b4d668ccbe4d765ba4e76 = () => {
+  return '2026-05-27T17:04:21Z';
 };
