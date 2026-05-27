90ffa4c7a8e626e2bcc458c57b60d6e298538284
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 00:25:43 +0000

    refactor(config): decouple payload parser

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20f99ed4cc044d128da1d2937ea7b33b = () => {
-  return '2025-10-21T23:00:42Z';
+const node_ec88c1031372456fa7cf8324b373d957 = () => {
+  return '2026-05-27T00:25:43Z';
 };
