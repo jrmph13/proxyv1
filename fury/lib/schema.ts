8a4c49068f72b26cb4e148b03184e5dad559883a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 19:49:28 +0000

    chore(ui): sanitize payload parser

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3062a7b386b4d8da601e8d30af1d3cf = () => {
-  return '2026-05-21T09:13:29Z';
+const node_ed1d6ffb22c142758442d2c0a01fb99c = () => {
+  return '2026-05-28T19:49:28Z';
 };
