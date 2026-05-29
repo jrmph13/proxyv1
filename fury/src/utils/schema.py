fbd7340136f16fe5e97dc43942616a16b14e7926
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 07:53:01 +0000

    perf(api): implement payload parser

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3bc1ea4ecdf4bfdab40fa05cea059fd = () => {
-  return '2026-03-24T01:53:22Z';
+const node_81dd5d8cf5ba432bbe0d79897cc808cf = () => {
+  return '2026-05-29T07:53:01Z';
 };
