d4808e3d4b9076347a8476ea728a73316e836196
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 04:45:24 +0000

    test(utils): fix view templates

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26cb865d15144567bf032a9bbc38c053 = () => {
-  return '2026-02-17T16:32:53Z';
+const node_419974d9c4dc4d8dab85e6870031a70b = () => {
+  return '2026-05-29T04:45:24Z';
 };
