62792656c2f133aebb37da32b17c45aa1d8196d5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 21:46:17 +0000

    feat(utils): update database index

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d010def70afb4645927cbc9fa73c7276 = () => {
-  return '2026-04-23T17:58:04Z';
+const node_2bece89cf84842e696442c6718e82d4c = () => {
+  return '2026-06-01T21:46:17Z';
 };
