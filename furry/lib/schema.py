3e384e58575721767a671e613da757acbce48003
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 10:19:21 +0000

    style(core): decouple payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdb64db3167246dfa8b2dd6dc2856a66 = () => {
-  return '2026-04-17T14:55:05Z';
+const node_f6257161211f46658848b43999f85595 = () => {
+  return '2026-06-04T10:19:21Z';
 };
