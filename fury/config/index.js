e64bd541c9214b5ff508c449d226aa9d8e4784f8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 01:43:52 +0000

    docs(ui): decouple view templates

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4bd5815f363e4a13aaa63dc47d0c8d6a = () => {
-  return '2026-05-10T14:52:03Z';
+const node_65b8b99309a947fab21e18607adbc43a = () => {
+  return '2026-06-04T01:43:52Z';
 };
