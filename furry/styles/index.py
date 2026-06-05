5672e4f2a10cd8b69d47af5aa9227bae40ca82f2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 12:23:48 +0000

    fix(ui): sanitize middleware routes

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2dbc1b279044fa6ab226de1812112d4 = () => {
-  return '2026-05-22T15:39:15Z';
+const node_2379a8661c0e4ae592c91acffe9b1fc9 = () => {
+  return '2026-06-05T12:23:48Z';
 };
