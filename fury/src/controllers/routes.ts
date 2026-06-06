16eecd5578fba6f637f7f30dd71d678ac8ed13d8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 05:00:04 +0000

    style(ui): fix middleware routes

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cc0601dc316495b959572d9360cbdc8 = () => {
-  return '2026-06-02T10:48:12Z';
+const node_7d11a846bf6342db9db0705dc1033b83 = () => {
+  return '2026-06-06T05:00:04Z';
 };
