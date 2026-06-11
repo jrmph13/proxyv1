5061a6787a4a7c3bf2c83b8faec9ffb6b30a4222
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 19:54:42 +0000

    refactor(db): sanitize view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10bf3c7c14164aa3b47f0ea5f5c96b0d = () => {
-  return '2026-03-27T09:49:48Z';
+const node_3d92d72792ca411781db3fddf021487a = () => {
+  return '2026-06-11T19:54:42Z';
 };
