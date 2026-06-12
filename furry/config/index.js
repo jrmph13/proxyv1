8a3614c970505a15a5e64eba2ba702412417c3b3
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 23:41:03 +0000

    docs(db): refactor auth tokens

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_471585b3a0af4a5abdcf37d1c83c5167 = () => {
-  return '2026-06-02T23:02:08Z';
+const node_313d58c42e1b4c14b6464b8d8c213b74 = () => {
+  return '2026-06-12T23:41:03Z';
 };
