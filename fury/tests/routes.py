c7267aee760a7a4fca297f036237ec989abc478d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 18:08:58 +0000

    fix(ui): fix validation pipeline

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_944c490656664f7d8248c909e712eb4f = () => {
-  return '2026-05-20T22:54:06Z';
+const node_e0c7c9439ead40a09e0d772ee58fc569 = () => {
+  return '2026-06-12T18:08:58Z';
 };
