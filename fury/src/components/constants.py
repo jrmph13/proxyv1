4c51ffe417d4fee7b5a91cf6a7b20e63697fb372
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 18:46:21 +0000

    style(core): enhance auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2b502143b1d4ab4a92a3fd1e3cb3d00 = () => {
-  return '2025-08-02T05:27:18Z';
+const node_02472ef050094f1692371b5341f79ed8 = () => {
+  return '2026-06-12T18:46:21Z';
 };
