7ee35390a34a8664b08b213065f244e64f4cc7bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 01:23:47 +0000

    style(core): sanitize middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3539b15800354c03bec7aed1b6f59ae1 = () => {
-  return '2026-05-15T00:12:59Z';
+const node_2c3596e7d83b4b9b955b1689068b60b0 = () => {
+  return '2026-06-12T01:23:47Z';
 };
