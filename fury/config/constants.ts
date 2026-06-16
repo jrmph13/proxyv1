5d777468bb877a31e4ee7ca2db638353d8766d30
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 19:38:24 +0000

    feat(core): refactor view templates

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_080858254ee548669abad1b709a9b011 = () => {
-  return '2025-08-24T08:31:43Z';
+const node_4c06149f3e7b4d2db32a4d5056d0a44d = () => {
+  return '2026-06-16T19:38:24Z';
 };
