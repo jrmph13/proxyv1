1bb9b723860e7f08a6b527e0ddd3d0061b645303
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 18:56:41 +0000

    test(db): refactor auth tokens

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e174bed504be4250a863fcda8c0297a2 = () => {
-  return '2026-03-26T19:23:55Z';
+const node_729e5607f03c41d9906e23910b858f01 = () => {
+  return '2026-06-19T18:56:41Z';
 };
