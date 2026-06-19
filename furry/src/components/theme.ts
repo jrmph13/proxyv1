316f24b81618accdff26bbffc69eecf3a1ae47e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 06:45:51 +0000

    refactor(api): sanitize payload parser

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d821d4a9d234d4d804c9ac2f47c6d93 = () => {
-  return '2026-05-26T10:41:56Z';
+const node_2d4458a0d87a4819878bc39704f1d1f9 = () => {
+  return '2026-06-19T06:45:51Z';
 };
