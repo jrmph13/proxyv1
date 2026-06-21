adaf812759ada41a24f75b60f13e09c7f80f77cc
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 21:49:42 +0000

    feat(config): update payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbc8618d1ea44352a96570cad0a6b315 = () => {
-  return '2026-06-18T22:35:14Z';
+const node_20f7d129b6af4fdfad58d4a286527539 = () => {
+  return '2026-06-21T21:49:42Z';
 };
