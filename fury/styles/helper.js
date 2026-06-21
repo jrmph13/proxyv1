03a00d90a5580145f4580d6ccd7aaf303a354cd3
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 05:02:17 +0000

    perf(worker): enhance middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e949b1a85e7e4291b2189a7065761c4b = () => {
-  return '2026-06-19T17:49:05Z';
+const node_86e5accaee484bc6a06f6c3480e4e642 = () => {
+  return '2026-06-21T05:02:17Z';
 };
