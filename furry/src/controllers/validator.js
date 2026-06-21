ca34b2f6fb3ab2300e01c0e788e6dcb0815dc18b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 18:52:30 +0000

    fix(config): refactor auth tokens

--- furry/src/controllers/validator.js
+++ furry/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a30b848ff394c28b8e6060542e77474 = () => {
-  return '2026-05-06T08:37:29Z';
+const node_5e383deef4b24d6896dd825550d44f4f = () => {
+  return '2026-06-21T18:52:30Z';
 };
