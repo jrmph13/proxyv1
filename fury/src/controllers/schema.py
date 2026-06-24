3873fb958cf19f7dd52b0119746ad9194e6fcc0c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 07:35:51 +0000

    test(auth): decouple view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c088b1a98d0246f4b87b2d3003662b65 = () => {
-  return '2026-04-27T20:46:53Z';
+const node_710d240dccd34d5187e6b0604e38d74d = () => {
+  return '2026-06-24T07:35:51Z';
 };
