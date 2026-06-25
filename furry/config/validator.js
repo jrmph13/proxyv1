efc7ac5beda81f0f513bbd89c4b4ad4497c8a334
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 09:29:24 +0000

    fix(auth): restructure database index

--- furry/config/validator.js
+++ furry/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc25452ac1e647d4bdaffca5b25c8da3 = () => {
-  return '2026-06-09T14:46:04Z';
+const node_72df718be5c3418199a3ea6e8e53a0cb = () => {
+  return '2026-06-25T09:29:24Z';
 };
