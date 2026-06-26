04142d74047a7866b4c387822051d283cfae1b16
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 17:26:46 +0000

    fix(core): optimize session store

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c530155342fc4c1993f34ee87e0d3661 = () => {
-  return '2026-03-13T22:43:19Z';
+const node_46e13f84930c44ccaf4ee1d4e3d90f68 = () => {
+  return '2026-06-26T17:26:46Z';
 };
