40d9d7c60c3867c41b707d9222627d818975f373
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 17:36:35 +0000

    fix(ui): optimize database index

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7df17132581405db9fe2b3ba8f3400f = () => {
-  return '2026-06-12T21:45:37Z';
+const node_f1eab64c7f954d699ddaf4e6e5c6d6cb = () => {
+  return '2026-07-02T17:36:35Z';
 };
