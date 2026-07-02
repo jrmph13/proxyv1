1c4b72021fe4c360acf8da7e52259dbb1a15f7ad
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 05:45:18 +0000

    docs(api): decouple caching layer

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_490e4070920c467ba9b7c2743782a0ec = () => {
-  return '2026-06-10T06:04:25Z';
+const node_606382f2c25141378d91828533369d99 = () => {
+  return '2026-07-02T05:45:18Z';
 };
