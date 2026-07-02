74be2acc7620c2de22325a40cb2ae59c80ba11f5
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 08:41:22 +0000

    refactor(utils): sanitize caching layer

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9759ccad044423f8ebef624cd07e211 = () => {
-  return '2026-06-24T14:16:00Z';
+const node_6c3cf495ab534627a8b65f0dd563c9a3 = () => {
+  return '2026-07-02T08:41:22Z';
 };
