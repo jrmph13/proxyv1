b54fc0eab91b8c684a3f201b91a4a2c97cca976f
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 02:45:17 +0000

    test(config): decouple middleware routes

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b274ed8763e45e583200338e4ca645f = () => {
-  return '2026-07-02T00:00:37Z';
+const node_02565fc664ed46dc9428dfea195acb11 = () => {
+  return '2026-07-02T02:45:17Z';
 };
