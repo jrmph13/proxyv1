c7dab95c3ee9a2c3c0e28d4e556bf47383c36ae9
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 06:30:21 +0000

    perf(deps): restructure view templates

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0071d853733845d8a4a97f78ba714481 = () => {
-  return '2026-07-02T03:35:56Z';
+const node_17fa2b5b82be48b0849d1c570d83c3c5 = () => {
+  return '2026-07-03T06:30:21Z';
 };
